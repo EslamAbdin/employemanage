45cc5d70427e438d860daaa0138c11cfe2eb9d85
EslamAbdin <eslamabdin2019@gmail.com>
2023-01-27 23:04:42 +0200

    Con.SetData(Query);

--- employemanage/Employees.cs
+++ employemanage/Employees.cs
@@ -191,6 +191,7 @@ namespace employemanage
                     int Salary = Convert.ToInt32(DailySalTb.Text);
                     string Query = "Update EmployeeTb1 set EmpName = '{0}',EmpGen='{1}',EmpDep={2},EmpDOB='{3}',Empjdate='{4}',EmpSal={5} where Empid= {6}";
                     Query = string.Format(Query, Name, Gender, Dep, DOB, JDate, Salary, key);
+                    Con.SetData(Query);
                 }
             }
 
