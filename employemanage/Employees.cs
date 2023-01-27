2bd7477421f62c32fe9e73780909587633e82aa2
EslamAbdin <eslamabdin2019@gmail.com>
2023-01-27 23:04:56 +0200

    MessageBox.Show("Emoloyee Updated!!!");

--- employemanage/Employees.cs
+++ employemanage/Employees.cs
@@ -192,6 +192,8 @@ namespace employemanage
                     string Query = "Update EmployeeTb1 set EmpName = '{0}',EmpGen='{1}',EmpDep={2},EmpDOB='{3}',Empjdate='{4}',EmpSal={5} where Empid= {6}";
                     Query = string.Format(Query, Name, Gender, Dep, DOB, JDate, Salary, key);
                     Con.SetData(Query);
+                    ShowEmp();
+                    MessageBox.Show("Emoloyee Updated!!!");
                 }
             }
 
