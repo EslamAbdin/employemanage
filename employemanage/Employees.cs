5930b151efbfb344c2fd89966931380b21142a9d
EslamAbdin <eslamabdin2019@gmail.com>
2023-01-27 23:03:59 +0200

    int Dep = Convert.ToInt32(DepCb.SelectedValue.ToString());

--- employemanage/Employees.cs
+++ employemanage/Employees.cs
@@ -185,6 +185,7 @@ namespace employemanage
                 {
                     string Name = EmpNameTb.Text;
                     string Gender = GenCb.SelectedItem.ToString();
+                    int Dep = Convert.ToInt32(DepCb.SelectedValue.ToString());
                 }
             }
 
