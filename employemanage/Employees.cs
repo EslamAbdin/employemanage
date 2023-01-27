31e7caf9648a5b0184cfe9900c211bb330d2a53c
EslamAbdin <eslamabdin2019@gmail.com>
2023-01-27 22:04:39 +0200

    add showemp object

--- employemanage/Employees.cs
+++ employemanage/Employees.cs
@@ -17,6 +17,7 @@ namespace employemanage
         {
             InitializeComponent();
             Con = new Functions();
+            ShowEmp();
             ShowDepartments();
         }
         private void ShowDepartments()
