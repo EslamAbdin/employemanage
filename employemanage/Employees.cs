967acec34d8235cccc953d7372ff520f199d9e6d
EslamAbdin <eslamabdin2019@gmail.com>
2023-01-27 22:33:09 +0200

    fourth

--- employemanage/Employees.cs
+++ employemanage/Employees.cs
@@ -128,5 +128,6 @@ namespace employemanage
          EmpNameTb.Text = EmployeeList.SelectedRows[0].Cells[1].Value.ToString();
          GenCb.Text = EmployeeList.SelectedRows[0].Cells[2].Value.ToString();
             DepCb.SelectedValue = EmployeeList.SelectedRows[0].Cells[3].Value.ToString();
+            DOBTb.Text = EmployeeList.SelectedRows[0].Cells[4].Value.ToString();
         }
 }
