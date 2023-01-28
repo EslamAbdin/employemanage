c1165040aea4dbecd1aaccfefd55594279b77888
EslamAbdin <eslamabdin2019@gmail.com>
2023-01-28 04:27:04 +0200

    start the del buttom

--- employemanage/Employees.cs
+++ employemanage/Employees.cs
@@ -139,7 +139,7 @@ namespace employemanage
             }
         }
 
-        private void DeleteBtn_Click(object sender, EventArgs e)
+         private void DeleteBtn_Click(object sender, EventArgs e)
         {
             try
             {
