cdbd8d509b7c0ec4d62b7b4c8c7787a0979abbbb
EslamAbdin <eslamabdin2019@gmail.com>
2023-01-28 04:50:00 +0200

    for the last time made sure that everything work fine and it is 100% fine

--- employemanage/Login.Designer.cs
+++ employemanage/Login.Designer.cs
@@ -154,7 +154,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(344, 390);
+            this.ClientSize = new System.Drawing.Size(350, 390);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
